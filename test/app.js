da7bceec2c503aaf2f804f3ae00c90a75ee96be6
Guille Paz <guille87paz@gmail.com>
2013-08-06 09:35:12 -0300

    Create test page.

---
+++ test/app.js
@@ -0,0 +1,17 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+	app = module.exports = express();
+
+/**
+ * App configuration.
+ */
+app.use(express.static(__dirname + '/'));
+app.use('/build', express.static(__dirname + '/../build'));
+app.use('/standalone', express.static(__dirname + '/../standalone'));
+
+/**
+ * Listen
+ */
+app.listen(8080);
